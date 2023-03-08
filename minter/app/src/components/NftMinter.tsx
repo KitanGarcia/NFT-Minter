9f4502a90431e0a68987a0bd2b21b8d29daaa27f
Kitan Garcia <kitangar@gmail.com>
2023-03-07 19:05:42 -0800

    add styling for minting page and mint button

--- minter/app/src/components/NftMinter.tsx
+++ minter/app/src/components/NftMinter.tsx
@@ -1,6 +1,7 @@
 import { PublicKey } from "@metaplex-foundation/js";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { LAMPORTS_PER_SOL, TransactionSignature } from "@solana/web3.js";
+import Image from "next/image";
 import { FC, useState } from "react";
 import { notify } from "../utils/notifications";
 
@@ -34,10 +35,46 @@ export const NftMinter: FC = () => {
     }
   };
 
+  const mintNFT = async () => {};
+
   return (
     <div>
-      <input type="file" onChange={uploadImage} />
-      <button>Mint NFT!</button>
+      <div className="mx-auto flex flex-col">
+        {createObjectUrl && (
+          <Image
+            className="mx-auto mb-4"
+            alt="uploadedImage"
+            width="300"
+            height="300"
+            src={createObjectUrl}
+          />
+        )}
+        {!mintAddress && !mintSignature && (
+          <div className="mx-auto text-center mb-2">
+            <input className="mx-auto" type="file" onChange={uploadImage} />
+          </div>
+        )}
+      </div>
+
+      <div className="flex flex-row justify-center">
+        <div className="relative group items-center">
+          {createObjectUrl &&
+            image &&
+            wallet &&
+            !mintAddress &&
+            !mintSignature && (
+              <div>
+                <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-orange-300 to-orange-500 rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
+                <button
+                  className="px-8 m-2 mt-4 w-40 h-14 btn animate-pulse bg-gradient-to-br from-orange-300 to-orange-500 hover:from-white hover:to-orange-300 text-black text-lg"
+                  onClick={mintNFT}
+                >
+                  <span>Mint!</span>
+                </button>
+              </div>
+            )}
+        </div>
+      </div>
     </div>
   );
 };
